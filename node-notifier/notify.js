6b8c46683e36ddd99927faf5ebfadb1da5189398
mixelpixel <mixelpix@mac.com>
2018-01-04 18:22:19 -0500

    USER branching: if win32, USERNAME

--- node-notifier/notify.js
+++ node-notifier/notify.js
@@ -2,4 +2,10 @@
 
 const notifier = require('node-notifier');
 
-notifier.notify(`What the hay, ${process.env.USER}?`);
+if (process.platform === 'win32') {
+    notifier.notify(`What the hay, ${process.env.USERNAME}?`);
+    console.log(`You're using: ${process.platform}`)
+  } else {
+    notifier.notify(`What the hay, ${process.env.USER}?`);
+    console.log(`You're using: ${process.platform}`)
+  }
