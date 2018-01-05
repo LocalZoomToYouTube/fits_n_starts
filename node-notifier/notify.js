1fde30db045845e67a38b68193dd722c476a0f71
mixelpixel <mixelpix@mac.com>
2018-01-04 21:10:51 -0500

    discern win32

--- node-notifier/notify.js
+++ node-notifier/notify.js
@@ -5,7 +5,7 @@ const notifier = require('node-notifier');
 if (process.platform === 'win32') {
     notifier.notify(`What the hay, ${process.env.USERNAME}?`);
     console.log(`You're using: ${process.platform}`)
-  } else {
-    notifier.notify(`What the hay, ${process.env.USER}?`);
-    console.log(`You're using: ${process.platform}`)
-  }
+} else {
+  notifier.notify(`What the hay, ${process.env.USER}?`);
+  console.log(`You're using: ${process.platform}`)
+}
